532cba76c9bb30819c92811c16d1762c53acbeb9
Tudor Daniel <tudordaniel56@gmail.com>
2023-10-30 09:53:52 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Page404 from "./pages/Page404";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Page404 />
+      <Routes>
+        <Route path = "/"/>
+        <Route path = "*" element = {<Page404 />} />
+      </Routes>
       <Footer />
     </div>
   );
