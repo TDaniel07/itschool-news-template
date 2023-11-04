c390caa07d413d6f667c637e03c33ee744d4df8a
Tudor Daniel <tudordaniel56@gmail.com>
2023-11-04 22:47:59 +0200

    removed a comment

--- src/endpoints/section.js
+++ src/endpoints/section.js
@@ -1,5 +1,4 @@
 import getApiKey from "./apiKey"
-//api key-ul este luat dintr-un fisier care nu este partajat pe github din motive de securitate.
 
 export default function getSection(section, nrOfArticles, page)
 {
